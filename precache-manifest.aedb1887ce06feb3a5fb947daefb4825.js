5ce7cc9ee3767cec8720df6f3a7ba4992c907f17
Bignichok <bignik545@gmail.com>
2020-09-19 08:54:32 +0300

    Updates

---
+++ precache-manifest.aedb1887ce06feb3a5fb947daefb4825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632fd69f9b2037da68fc29a64fb378f5",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "b2d3bb31790ca2d66f21",
+    "url": "/react-hw-01-components/static/css/main.52471071.chunk.css"
+  },
+  {
+    "revision": "e994a5b3a5ee7dcb5b4b",
+    "url": "/react-hw-01-components/static/js/2.2e37d07e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-01-components/static/js/2.2e37d07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d3bb31790ca2d66f21",
+    "url": "/react-hw-01-components/static/js/main.dfe7527b.chunk.js"
+  },
+  {
+    "revision": "6fe3cc52a8b0257f05d9",
+    "url": "/react-hw-01-components/static/js/runtime-main.459b7fff.js"
+  }
+]);
